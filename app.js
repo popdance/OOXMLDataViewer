c2189a253ba7a30a2b84a269d8a673e521c79bba
popdance <popdance@github.com>
2015-03-02 14:43:02 +0900

    first commit!

---
+++ app.js
@@ -0,0 +1,9 @@
+﻿chrome.app.runtime.onLaunched.addListener(function() {
+	chrome.app.window.create('app.html', {
+		'bounds': {
+			'width': Math.round(window.screen.availWidth*0.9),
+            'height': Math.round(window.screen.availHeight*0.9)
+		},
+		resizable:true
+	});
+});
